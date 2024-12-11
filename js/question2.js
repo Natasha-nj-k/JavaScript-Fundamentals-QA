a8f9a3b3e2160c3992910552937e78a33c447e07
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-11 16:16:14 +0300

    completed q2

--- js/question2.js
+++ js/question2.js
@@ -1,2 +1,5 @@
 // Question2: How can you include JavaScript in an HTML file? (Write an example.)
 
+// Internal - <script></script>
+// In-line - <h1 onclick{}>Hello, World</h1>
+// External - <script src="./Scripts/index.js"></script>
