e8533efe82a243c73023d496a99881cdf869315e
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-11 17:01:22 +0300

    completed q3

---
+++ js/question3.js
@@ -0,0 +1,4 @@
+// Question3: What are variables in JavaScript, and how do you declare them?
+
+// Variables in Js are used to store data or info that can be refered to and manipulate later in the program.
+// They can be declared by using; let and const.
