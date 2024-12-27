e32ad1b11db2b3af5361481b008bd738fb2983bd
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:46:02 +0300

    completed q11

--- js/question11.js
+++ js/question11.js
@@ -1,1 +1,4 @@
 //Question11: What is the difference between == and === in JavaScript?
+
+// ==: Compares values for equality after type conversion.
+//===: Compares values and types for strict equality.
