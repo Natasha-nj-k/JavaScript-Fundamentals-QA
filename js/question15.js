57920d4701da5b22ce514346676fb219287e44b6
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:46:55 +0300

    completed q15

--- js/question15.js
+++ js/question15.js
@@ -1,1 +1,5 @@
 //Question15: Write a JavaScript expression that evaluates whether a number is both greater than 10 and less than 20.
+
+// let num = 15;
+// let result = num > 10 && num < 20;
+// console.log(result);
