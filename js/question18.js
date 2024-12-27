25d56df43ad8ff4d3f5398a943ef35dca9aa9eb8
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:47:22 +0300

    completed q18

--- js/question18.js
+++ js/question18.js
@@ -1,1 +1,4 @@
 //Question18: What is the difference between function declarations and function expressions?
+
+// Function Declaration: Can be called before it's defined.
+// Function Expression: Assigned to a variable; cannot be called before definition.
