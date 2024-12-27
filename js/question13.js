5f21f5f501d7c241fb03bd528b54f9797892343f
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:46:28 +0300

    completed q13

--- js/question13.js
+++ js/question13.js
@@ -1,1 +1,5 @@
 //Question13: List and explain three arithmetic operators in JavaScript.
+
+// Addition (+): Adds numbers or concatenates strings.
+// Subtraction (-): Subtracts one number from another.
+// Multiplication (*): Multiplies two numbers
