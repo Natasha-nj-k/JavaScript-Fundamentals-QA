2fdbe1a855c0504db4b5742927c87631b956c690
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:47:41 +0300

    completed q20

--- js/question20.js
+++ js/question20.js
@@ -1,1 +1,4 @@
 //Question20: What are arrow functions, and how are they different from regular functions?
+
+//Arrow functions provide a concise syntax and do not bind their own this.
+//const add = (a, b) => a + b;
