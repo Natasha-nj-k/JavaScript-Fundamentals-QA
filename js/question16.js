66f9021a827186c3448960af650110ab3e9295f2
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:47:02 +0300

    completed q16

--- js/question16.js
+++ js/question16.js
@@ -1,1 +1,6 @@
 //Question16: What is a function in JavaScript, and how do you define one?
+
+//A function is a block of code designed to perform a task. Eg:
+   //function greet(name) {
+  //   return "Hello, " + name;
+  // }
