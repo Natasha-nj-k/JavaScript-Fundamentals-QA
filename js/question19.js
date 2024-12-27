075dff1f35bc2748ffefe41bcdfd088c0c66c491
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:47:32 +0300

    completed q19

--- js/question19.js
+++ js/question19.js
@@ -1,1 +1,4 @@
 //Question19: Explain the concept of scope in JavaScript.
+
+//Function Declaration: Can be called before it's defined.
+// Function Expression: Assigned to a variable; cannot be called before definition.
