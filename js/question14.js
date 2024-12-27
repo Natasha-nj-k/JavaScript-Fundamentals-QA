b77e2b46c99d6e497f717951d45cb02fe0bc7686
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:46:45 +0300

    completed q14

--- js/question14.js
+++ js/question14.js
@@ -1,1 +1,4 @@
 //Question14: What is the difference between logical operators && and ||?
+
+// && (AND): Returns true if both conditions are true.
+// || (OR): Returns true if at least one condition is true.
