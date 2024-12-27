9dfd62fc4d206ef54e071f75d1dd86d98ad16623
Natasha-n-j <natashanjkaranja@gmail.com>
2024-12-27 03:46:09 +0300

    completed q12

--- js/question12.js
+++ js/question12.js
@@ -1,3 +1,6 @@
 //Question12: What will the following code output, and why?
    //console.log(2 + "2");
   // console.log(2 - "2");
+
+  // 2 + "2": "22" (string concatenation).
+  //2 - "2": 0 (subtraction converts the string to a number)
